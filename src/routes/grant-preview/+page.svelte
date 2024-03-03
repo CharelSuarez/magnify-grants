3245ade57d88d2469d697f6c6994fd36090e1250
michael <michaelzhou20111@hotmail.com>
2024-03-03 14:26:20 -0500

    added grant form preview page

---
+++ src/routes/grant-preview/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card";
+</script>
+   
+<Card.Root>
+    <Card.Header>
+      <Card.Title>awesome grant</Card.Title>
+      <Card.Description>grant Description</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <Card.Title>organization</Card.Title>
+      <p>awesome granting org</p>
+    </Card.Content>
+    <Card.Content>
+      <Card.Title>short answer</Card.Title>
+      <p>f & l name</p>
+    </Card.Content>
+    <Card.Content>
+      <Card.Title>paragraph</Card.Title>
+      <p>tell ur life story</p>
+    </Card.Content>
+    <Card.Content>
+      <Card.Title>multiple choice</Card.Title>
+      <p>are you smart</p>
+      <p>are you dumb</p>
+      <p>select this</p>
+    </Card.Content>
+</Card.Root>
