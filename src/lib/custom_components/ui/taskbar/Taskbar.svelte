20a0c135bac4d35372c3dec5c7d6a77f1e43f816
Abejithp <abejithp@hotmail.com>
2024-03-03 16:49:02 -0500

    created intial landing page

---
+++ src/lib/custom_components/ui/taskbar/Taskbar.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Button } from "$lib/components/ui/button";
+    import image from "$lib/assets/branding/images/magnify_logo.png";
+</script>
+
+<div class="flex justify-between px-10 py-5 shadow-lg items-center">
+    <div class="flex items-center">
+        <img src={image} alt="logo" class="h-12"/>
+    </div>
+    <div class="flex items-center">
+        
+        <Button class="mr-5" href="https://www.magnifyaccess.ai/">
+            Home
+        </Button>
+        <Button class="mr-5" href="/signup">Login</Button>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Taskbar from '$lib/custom_components/ui/taskbar/Taskbar.svelte';
+	import image from '$lib/assets/branding/images/magnify_image.webp';
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<main class="flex-col h-auto">
+	<Taskbar />
+
+	<div class="flex justify-evenly items-center h-screen">
+		<div class="flex-col justify-center items-center w-auto">
+			<h1 class="text-6xl font-bold max-sm:text-lg">Grant Service</h1>
+			<p class="text-lg my-10 w-[550px] max-sm:text-sm ">
+				Empowering organizations to fulfill their missions through comprehensive grant support services, driving impactful change and sustainable growth in our communities.
+			</p>
+
+			<div class="flex">
+				<Button class="mr-5" href="https://www.magnifyaccess.ai/contact-us">Contact Us</Button>
+			</div>
+		</div>
+
+		<img class="z-50 hidden xl:inline" src={image} alt="Magnify Access Branding" />
+	</div>
+
+    
+    <div class="flex justify-evenly items-center h-screen max-sm:flex-col">
+        <Card.Root>
+            <Card.Header>
+              <Card.Title>
+                <p class="text-center">Feature #1</p>
+              </Card.Title>
+            </Card.Header>
+            <Card.Content>
+                <img src={image} class="w-[250px]" alt="" srcset="">
+            </Card.Content>
+            <Card.Footer>
+              <p class="w-[250px] text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, modi.
+              </p>
+            </Card.Footer>
+        </Card.Root>
+
+        <Card.Root>
+            <Card.Header>
+              <Card.Title>
+                <p class="text-center">Feature #2</p>
+              </Card.Title>
+            </Card.Header>
+            <Card.Content>
+                <img src={image} class="w-[250px]" alt="" srcset="">
+            </Card.Content>
+            <Card.Footer>
+              <p class="w-[250px] text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, modi.
+              </p>
+            </Card.Footer>
+        </Card.Root>
+
+        <Card.Root>
+            <Card.Header>
+              <Card.Title>
+                <p class="text-center">Feature #3</p>
+              </Card.Title>
+            </Card.Header>
+            <Card.Content>
+                <img src={image} class="w-[250px]" alt="" srcset="">
+            </Card.Content>
+            <Card.Footer>
+              <p class="w-[250px] text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, modi.
+              </p>
+            </Card.Footer>
+        </Card.Root>
+	</div>
+
+
+	
+</main>
