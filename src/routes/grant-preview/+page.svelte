57fbaa0e5216d831f3ae3405e0129f999b9e892f
michael <michaelzhou20111@hotmail.com>
2024-03-03 16:13:15 -0500

    update preview page

--- src/routes/grant-preview/+page.svelte
+++ src/routes/grant-preview/+page.svelte
@@ -1,28 +1,49 @@
 <script lang="ts">
-    import * as Card from "$lib/components/ui/card";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+
 </script>
-   
-<Card.Root>
-    <Card.Header>
-      <Card.Title>awesome grant</Card.Title>
-      <Card.Description>grant Description</Card.Description>
-    </Card.Header>
-    <Card.Content>
-      <Card.Title>organization</Card.Title>
-      <p>awesome granting org</p>
-    </Card.Content>
-    <Card.Content>
-      <Card.Title>short answer</Card.Title>
-      <p>f & l name</p>
-    </Card.Content>
-    <Card.Content>
-      <Card.Title>paragraph</Card.Title>
-      <p>tell ur life story</p>
-    </Card.Content>
-    <Card.Content>
-      <Card.Title>multiple choice</Card.Title>
-      <p>are you smart</p>
-      <p>are you dumb</p>
-      <p>select this</p>
-    </Card.Content>
-</Card.Root>
+
+<div class="center">
+    <Card.Root>
+        <Card.Header >
+          <Card.Title>Grant Title</Card.Title>
+        </Card.Header>
+        <Card.Content >
+          <Card.Title>Organization</Card.Title>
+          <p>Organization Name</p>
+        </Card.Content>
+        <Card.Content>
+          <Card.Title>Short Answer</Card.Title>
+          <p>Question 1</p>
+        </Card.Content>
+        <Card.Content>
+          <Card.Title>Paragraph</Card.Title>
+          <p>Write anything</p>
+        </Card.Content>
+        <Card.Content>
+          <Card.Title>Multiple Choice</Card.Title>
+          <p>Question 1</p>
+          <p>Question 2</p>
+          <p>Question 3</p>
+        </Card.Content>
+    
+        <Card.Footer class="flex justify-between">
+            <Button variant="outline">Go back</Button>
+            <Button>Edit</Button>
+          </Card.Footer>
+    </Card.Root>
+</div>
+
+
+<style>
+    .center {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        padding: 10px;
+        margin: auto;
+        width: 30%;
+    }
+</style>
