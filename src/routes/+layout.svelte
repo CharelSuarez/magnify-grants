6a087dae3a2488646b42584293852b3419b8dbf2
Nathanael Robbins <nejrobbins@gmail.com>
2024-03-03 15:18:33 -0500

    SCRUM-29: Add interface to view list of grants

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-    import "../app.pcss";
-    import { page } from "$app/stores";
-    import { getFlash } from "sveltekit-flash-message";
-    import { Toaster } from "$lib/components/ui/sonner";
-    import { toast } from "svelte-sonner";
-    import { setMode } from "mode-watcher";
+	import '../app.pcss';
+	import { page } from '$app/stores';
+	import { getFlash } from 'sveltekit-flash-message';
+	import { Toaster } from '$lib/components/ui/sonner';
+	import { toast } from 'svelte-sonner';
+	import { setMode } from 'mode-watcher';
 
-    setMode("light");
+	setMode('light');
 
-    const flash = getFlash(page);
+	const flash = getFlash(page);
 
-    let richColors = false;
+	let richColors = false;
 
-    $: if ($flash) {
+	$: if ($flash) {
 
-        richColors = $flash.richColors == null ? false : $flash.richColors ? true : false;
+		richColors = $flash.richColors == null ? false : $flash.richColors ? true : false;
 
-        if ($flash.type === "message") {
-            toast.message($flash.message, {
-                description: $flash.description
-            });
-        } else if ($flash.type === "success") {
-            toast.success($flash.message, {
-                description: $flash.description
-            });
-        } else if ($flash.type === "error") {
-            toast.error($flash.message, {
-                description: $flash.description
-            });
-        }
+		if ($flash.type === 'message') {
+			toast.message($flash.message, {
+				description: $flash.description
+			});
+		} else if ($flash.type === 'success') {
+			toast.success($flash.message, {
+				description: $flash.description
+			});
+		} else if ($flash.type === 'error') {
+			toast.error($flash.message, {
+				description: $flash.description
+			});
+		}
 
-    }
+	}
 
 
 </script>
 
 <slot />
 
-<Toaster {richColors}/>
+<Toaster {richColors} />
---
+++ src/routes/grants/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import DataTable from './data-table.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	let grants = data.response;
+</script>
+
+<div class="container mx-auto py-10">
+	<DataTable grants={grants} />
+</div>
