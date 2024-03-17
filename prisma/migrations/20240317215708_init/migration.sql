70c3c700ccb6488c7558d8339881e1dc8d33ddb1
michael <michaelzhou20111@hotmail.com>
2024-03-17 18:22:51 -0400

    update preview page branch with main

---
+++ prisma/migrations/20240317215708_init/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - The values [MultipleChoiceGrid,CheckboxGrid] on the enum `FieldType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "FieldType_new" AS ENUM ('ShortAnswer', 'Paragraph', 'MultipleChoice', 'Checkbox', 'Dropdown', 'FileUpload', 'LinearScale', 'Date', 'Time');
+ALTER TABLE "fields" ALTER COLUMN "type" TYPE "FieldType_new" USING ("type"::text::"FieldType_new");
+ALTER TYPE "FieldType" RENAME TO "FieldType_old";
+ALTER TYPE "FieldType_new" RENAME TO "FieldType";
+DROP TYPE "FieldType_old";
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "fields" ADD COLUMN     "values2" TEXT[];
