15a2b09786c28e69d3cc38dcb91160084300f84e
Lucas I <lucas1.ilea@gmail.com>
2024-03-28 21:26:11 -0400

    make download/upload util and upload req docs (#58)

--- src/lib/utils/downloads.ts
+++ src/lib/utils/downloads.ts
@@ -8,20 +8,33 @@ export const downloadBanner = async (grantId: string, supabase: SupabaseClient) 
 	return await download(`${grantId}.png`, 'banners', supabase);
 };
 
+export const listGrantDocuments = async (grantId: string, supabase: SupabaseClient) => {
+	return await supabase.storage.from('documents').list(grantId);
+};
+
+export const downloadGrantDocument = async (
+	grantId: string,
+	fileName: string,
+	supabase: SupabaseClient
+) => {
+	return await download(`${grantId}/${fileName}`, 'documents', supabase);
+};
+
 export const downloadRequiredDocument = async (
 	userId: string,
 	docId: string,
 	supabase: SupabaseClient
 ) => {
-	const { data, error } = await supabase.storage
+	const { data, errors } = await supabase.storage
 		.from('required_documents')
 		.list(`${userId}/${docId}`, {
 			limit: 1
 		});
 
 	if (!data) {
-		return { data, error };
+		console.log("Couldn't find document");
+		return { data, errors };
 	}
 
-	return await download(`${userId}/${docId}/${file.name}`, 'required_documents', supabase);
+	return await download(`${userId}/${docId}/${data[0].name}`, 'required_documents', supabase);
 };
