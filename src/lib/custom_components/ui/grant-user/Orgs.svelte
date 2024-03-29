cdbee8423e9923bdbdc5c2e19bd6932cc9bb9210
Abejithp <abejithp@hotmail.com>
2024-03-29 00:57:21 -0400

    styled org

--- src/lib/custom_components/ui/grant-user/Orgs.svelte
+++ src/lib/custom_components/ui/grant-user/Orgs.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import image from '$lib/assets/branding/images/rbc-grant-template.jpg';
+	import circle_500 from '$lib/assets/abstract/circle-500.png';
+	import circle_700 from '$lib/assets/abstract/circle-700.png';
+	import circle_800 from '$lib/assets/abstract/circle-800.png';
+	import logo from '$lib/assets/branding/images/rbc-icon-template.png';
 	import { t } from '$lib/i18n/i18n';
 
 	export let name: string;
@@ -15,16 +18,22 @@
 
 <Button
 	class="flex h-[350px] bg-white border-4 border-teal-500 shadow-xl justify-start p-4 w-[1fr] min-w-[250px] rounded-lg text-black cursor-pointer hover:scale-90
-    transition-all duration-500 ease-out flex-col hover:bg-white relative"
+    transition-all duration-500 ease-out flex-col hover:bg-white relative overflow-hidden "
 	on:click={handleClick}
 >
-	<h1 class="flex w-full text-left text-[2rem] h-fit break-normal text-wrap leading-10 z-10">
-		{name}
-	</h1>
+	<div class="flex w-full gap-4 mb-4">
+		<img src={logo} alt="" class="h-[40px] self-center" />
+		<h1 class="flex w-full text-left text-[3rem] h-fit break-normal text-wrap leading-10 z-10">
+			{name}
+		</h1>
+	</div>
+
 	<div class="flex w-full gap-2 font-bold text-[1rem] mt-4">
-		<p class="text-left break-all z-10">{$t('label.description')}: </p>
+		<p class="text-left break-all z-10 text-teal-700">{$t('label.description')}:</p>
 		<p class=" text-ellipsis overflow-hidden font-normal">{description}</p>
 	</div>
-	
-	<img src={image} alt="Grant Banner" class=" absolute w-full p-4 bottom-0 max-h-full rounded-2xl">
+
+	<img src={circle_500} alt="" class=" absolute bottom-[-25%] left-[-25%] z-20" />
+	<img src={circle_700} alt="" class=" absolute bottom-[-30%] left-[0] z-10" />
+	<img src={circle_800} alt="" class=" absolute bottom-[-35%] left-[25%] z-0" />
 </Button>
