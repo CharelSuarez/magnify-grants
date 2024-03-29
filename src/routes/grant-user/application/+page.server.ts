1091122ad022f1867e6026e4ae7f347cd7ccfe78
abejith <abejithp@hotmail.com>
2024-03-29 16:28:30 -0400

    added page view for user status -added status component

---
+++ src/routes/grant-user/application/+page.server.ts
@@ -0,0 +1,47 @@
+import { goto } from '$app/navigation';
+import { db } from '$lib/server/db';
+import { redirect, setFlash } from 'sveltekit-flash-message/server';
+import type { PageServerLoad } from './$types';
+
+/** @type {import('./$types').PageServerLoad} */
+
+export const load: PageServerLoad = async (event) => {
+
+    if(!event.locals.user){
+        redirect(
+            307,
+            `/grant-user`, 
+            {
+                richColors: true,
+                type: 'error',
+                message: 'Not Logged In!'
+            },
+            event
+        );
+    }
+
+    const user = event.locals.user?.id
+
+	const submission = await db.submission.findMany({
+        where : {
+            userId: user
+        },
+        include: {
+            grant: {
+                select:{
+                    title: true,
+                    description: true,
+                    expirationDate:true,
+                }
+            }
+        }
+	});
+
+
+
+
+	return {
+		grants: submission || [],
+	};
+};
+
